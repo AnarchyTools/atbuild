47f4d2db3895299f77a11d7be96df409ee2cb3d4
Drew Crawford <drew@sealedabstract.com>
2016-01-13 08:31:37 -0600

    Don't allow build to proceed if the external tool failed

--- src/Shell.swift
+++ src/Shell.swift
@@ -4,5 +4,8 @@ final class Shell : Tool {
         guard let script = args["script"]?.string else { throw AnarchyBuildError.CantParseYaml("Invalid 'script' argument to shell tool.") }
         let t = NSTask.launchedTaskWithLaunchPath("/bin/sh", arguments: ["-c",script])
         t.waitUntilExit()
+        if t.terminationStatus != 0 {
+            throw AnarchyBuildError.ExternalToolFailed("/bin/sh -c \(script)")
+        }
     }
 }

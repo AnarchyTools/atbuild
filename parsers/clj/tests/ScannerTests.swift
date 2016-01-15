efc5045d22b353ef5e71e526998447bb557ff8c9
David Owens II <david@owensd.io>
2016-01-14 16:31:52 -0800

    Added the appropriate apache lic header.

--- parsers/clj/tests/ScannerTests.swift
+++ parsers/clj/tests/ScannerTests.swift
@@ -1,9 +1,17 @@
-//  © 2016 Anarchy Tools Contributors.
-//  This file is part of atbuild.  It is subject to the license terms in the LICENSE
-//  file found in the top level of this distribution
-//  No part of atbuild, including this file, may be copied, modified,
-//  propagated, or distributed except according to the terms contained
-//  in the LICENSE file.
+// Copyright (c) 2016 Anarchy Tools Contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 import Foundation
 

3c42c6361f76d0e63b79e3d28bfd5bf5f3e08c24
Drew Crawford <drew@sealedabstract.com>
2016-01-15 07:10:20 -0600

    Resolve a CI failure in the previous commit
    We have to make these directories manually in the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir -p .atllbuild/objects
+mkdir -p .atllbuild/products
 swift-build-tool -f yaml-osx-llbuild.yaml
 swift-build-tool -f llbuild-osx.yaml
 cp .atllbuild/products/atbuild .

829dbf7f39cbc37be834ca2098a0c83c5cf6a851
Drew Crawford <drew@sealedabstract.com>
2016-01-16 11:27:55 -0600

    Release version 0.2.0

--- atbuild/src/main.swift
+++ atbuild/src/main.swift
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-let version = "0.2.0-dev"
+let version = "0.2.0"
 
 import Foundation
 import atpkg

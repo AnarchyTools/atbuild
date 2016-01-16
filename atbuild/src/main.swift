60251d6c8f7d63b8e3e9e01b75a7f33e3bfba1ba
Drew Crawford <drew@sealedabstract.com>
2016-01-16 11:28:18 -0600

    Mark 0.3.0-dev

--- atbuild/src/main.swift
+++ atbuild/src/main.swift
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-let version = "0.2.0"
+let version = "0.3.0-dev"
 
 import Foundation
 import atpkg

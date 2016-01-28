f6691e917834b8d66a7377b998384481d676f4a4
Drew Crawford <drew@sealedabstract.com>
2016-01-27 21:22:41 -0600

    Update release script for new CI artifact format

--- release.sh
+++ release.sh
@@ -1,17 +1,19 @@
-if [ ! -f artifacts-linux.tgz ]; then
-    echo "Missing artifacts-linux.tgz"
+if [ ! -f artifacts-linux.zip ]; then
+    echo "Missing artifacts-linux.zip"
     exit 1
 fi 
-if [ ! -f artifacts-osx.tgz ]; then
-    echo "Missing artifacts-osx.tgz"
+if [ ! -f artifacts-osx.zip ]; then
+    echo "Missing artifacts-osx.zip"
     exit 1
 fi 
 echo "version to tag:"
 read version
-tar xf artifacts-linux.tgz
-tar c bin/ | pixz -9 -o atbuild-${version}-linux.tar.xz
+rm -rf /tmp/build
+unzip artifacts-linux.zip -d /tmp/build/
+tar c -C /tmp/build bin | pixz -9 -o atbuild-${version}-linux.tar.xz
 #the OSX on the other hand is in .atllbuild
+rm -rf /tmp/build
 mkdir -p /tmp/build/bin
-tar xf artifacts-osx.tgz -C /tmp/build/
+unzip artifacts-osx.zip -d /tmp/build/
 cp /tmp/build/.atllbuild/products/atbuild /tmp/build/bin/atbuild
 tar c -C /tmp/build bin | pixz -9 -o atbuild-${version}-macosx.tar.xz

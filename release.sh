35943a08e3714a40f68f607b6304de6472ea213c
Drew Crawford <drew@sealedabstract.com>
2016-02-05 19:54:04 -0600

    Bump version to 0.8.0-dev

--- release.sh
+++ release.sh
@@ -11,9 +11,7 @@ read version
 rm -rf /tmp/build
 unzip artifacts-linux.zip -d /tmp/build/
 tar c -C /tmp/build bin | pixz -9 -o atbuild-${version}-linux.tar.xz
-#the OSX on the other hand is in .atllbuild
 rm -rf /tmp/build
-mkdir -p /tmp/build/bin
+mkdir -p /tmp/build
 unzip artifacts-osx.zip -d /tmp/build/
-cp /tmp/build/.atllbuild/products/atbuild /tmp/build/bin/atbuild
 tar c -C /tmp/build bin | pixz -9 -o atbuild-${version}-macosx.tar.xz
